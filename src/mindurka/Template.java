8f205b5f7beadf02ef3b57d11bc92e00409738bd
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-06-09 13:44:20 +0300

    Залил префиксы

--- src/mindurka/Template.java
+++ src/mindurka/Template.java
@@ -1,19 +1,58 @@
 package mindurka;
 
 import arc.util.*;
+import arc.*;
+
+import mindustry.core.*;
+import mindustry.game.*;
 import mindustry.mod.*;
+import mindustry.gen.*;
+import mindustry.Vars;
 
 public class Template extends Plugin {
 
     @Override
     public void init() {
+
+        Events.on(EventType.PlayerChatEvent.class, event -> {
+            if (!event.message.startsWith("/")) {
+                String prefix = event.player.admin() ? "[scarlet]Админ[white] | " : "[cyan]Игрок[white] | ";
+                String playerName = NetClient.colorizeName(event.player.id, event.player.name);
+                Call.sendMessage(prefix + playerName + " [gold]>[white] " + event.message);
+                Log.info(event.player.name + " > " + event.message);
+            }
+        });
+
+        Vars.netServer.admins.addChatFilter((player, text) -> null);
     }
 
+    //register commands that player can invoke in-game
     @Override
-    public void registerClientCommands(CommandHandler handler) {
-    }
+    public void registerClientCommands(CommandHandler handler){
+        handler.removeCommand("a");
+        handler.removeCommand("t");
+        handler.<Player>register("a", "<текст...>", "Отправить сообщение от имени админа", (args, player) -> {
+            if (!player.admin()) {
+                player.sendMessage("[scarlet]Сначала купи админку))0)!");
+                return;
+            }
 
-    @Override
-    public void registerServerCommands(CommandHandler handler) {
+            String message = args[0];
+            String prefix = "[scarlet]Админ[] | ";
+            String playerName = NetClient.colorizeName(player.id, player.name);
+
+            Groups.player.each(Player::admin, otherPlayer -> {
+                otherPlayer.sendMessage("<[scarlet]A[]>" + prefix + playerName + " [gold]>[#ff4449] " + message);
+            });
+        });
+        handler.<Player>register("t", "<текст...>", "Отправить командное сообщение", (args, player) -> {
+            String message = args[0];
+            String playerName = NetClient.colorizeName(player.id, player.name);
+            String prefix = player.admin() ? "[scarlet]Админ[white] | " : "[cyan]Игрок[white] | ";
+
+            Groups.player.each(o -> o.team() == player.team(), otherPlayer -> {
+                otherPlayer.sendMessage("<[#" + player.team().color + "]T[]>" + prefix + playerName + " [gold]>[lime] " + message);
+            });
+        });
     }
 }
